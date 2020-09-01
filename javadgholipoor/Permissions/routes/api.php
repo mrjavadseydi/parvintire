20060f89712a95cd48ccb48cbf844dea024f943e
javadgholipoor <apkmaker74@gmail.com>
2020-09-01 10:21:12 +0430

    api:auth

--- javadgholipoor/Permissions/routes/api.php
+++ javadgholipoor/Permissions/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-Route::group(['middleware' => 'auth:api'], function () {
+Route::group(['middleware' => 'api:auth'], function () {
     Route::group(['prefix' => 'v1'], function () {
         Route::get('permissions/sync', 'PermissionController@sync');
         Route::resource('permissions', 'PermissionController');
